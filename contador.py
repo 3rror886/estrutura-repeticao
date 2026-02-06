b66d1c456903f44d5a282cc4b7bf280bca1ad68d
Vinicius Augusto Vargas <viniciusaugusto1006@gmail.com>
2026-02-05 21:31:26 -0300

    contador-repetidor

---
+++ contador.py
@@ -0,0 +1,17 @@
+'''
+
+Estrutura de repetição
+
+- Serve para executar um bloco de codigo varias vezes.
+- APENAS ENQUANTO UMA CONDIÇÃO FOR VERDADEIRA.
+
+'''
+
+#variavel = True #Boolean
+
+tabuada = 9
+contador = 0
+
+while contador <= 10:
+    print("eu vou contar, olha só ", tabuada, "X", contador, "=", contador * tabuada)
+    contador += 1

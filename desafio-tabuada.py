de77059adf7e681c1b6a3961593f36c6811856b3
Vinicius Augusto Vargas <viniciusaugusto1006@gmail.com>
2026-02-06 19:39:30 -0300

    desafio-completo

--- desafio-tabuada.py
+++ desafio-tabuada.py
@@ -1,20 +1,27 @@
 #"Parado ai visitante"
 
-tabuada = int(input("digite a tabuada que você deseja: "))
-contador = 0
-extensão = int(input("digite até a onde você deseja que sua tabuada siga: "))
+while True:
+    tabuada = int(input("digite a tabuada que você deseja: "))
+    contador = 0
+    extensão = int(input("digite até a onde você deseja que sua tabuada siga: "))
 
-sim = True
-nao = False
+    if tabuada < 0:
+        print("errado numeros negativos não são permitidos")
+    elif contador < 0:
+        print("errado numeros negativos não são permitidos")
+        continue
 
-if tabuada < 0:
-    print("errado numeros negativos não são permitidos")
-elif contador < 0:
-    print("errado numeros negativos não são permitidos")
-else:
     while contador <= extensão:
         print(tabuada, "x", contador, "=", contador * tabuada)
         contador += 1
     
+    print("sim ou não" )
+    escolha = input("selecione uma nova tabuada: ")
+
+    if escolha == "sim":
+        print("receba")
+    else:
+        print("nah")
+        break
 
 
